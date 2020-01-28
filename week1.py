f1ffb5a8055ae6d928bcd506e1594674ea8bf8e4
earthsaharat <saharat.earth@gmail.com>
2020-01-28 15:45:29 +0700

    change format

---
+++ week1.py
@@ -0,0 +1,18 @@
+
+def alphabet_position(X):
+	ans = ""
+	for x in X:
+		xInt = ord(x)
+		if 97 <= xInt and xInt <= 122: 
+			xInt = xInt-96
+			ans += str(xInt)+' '
+		elif 65 <= xInt and xInt <= 90: 
+			xInt = xInt-64
+			ans += str(xInt)+' '
+	return ans
+
+# print(alphabet_position("The sunset sets at twelve o'clock"))
+
+# a = input("input hear : ")
+
+# print(a)
