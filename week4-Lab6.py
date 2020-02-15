c15804c8a6e854397cfde1413366274f78e70ca9
earthsaharat <saharat.earth@gmail.com>
2020-02-15 16:12:30 +0700

    update week4

---
+++ week4-Lab6.py
@@ -0,0 +1,11 @@
+import cv2
+import numpy as np
+
+img = cv2.imread("container.jpg", cv2.IMREAD_COLOR)
+img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+ret,thresh1 = cv2.threshold(img_gray,200,255,cv2.THRESH_BINARY)
+
+cv2.imshow('original',img)
+cv2.imshow('threshold',thresh1)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
